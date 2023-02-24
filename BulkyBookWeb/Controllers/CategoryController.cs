4a186516f565f799d1fb8bd00082a9f9cf61b619
Ameya G Mahale <ameya.mahale@iifl.com>
2023-02-24 18:16:14 +0530

    Display All Categories

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
@@ -14,8 +15,8 @@ namespace BulkyBookWeb.Controllers
 
         public IActionResult Index()
         {
-            var objContextList = _db.Categories.ToList();
-            return View();
+            IEnumerable<Category> objContextList = _db.Categories;
+            return View(objContextList);
         }
     }
 }
